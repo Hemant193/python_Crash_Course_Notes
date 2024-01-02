96861c2961763158b6406e5f464ff9123194e08d
Hemant <hemishra555@gmail.com>
2024-01-02 15:59:25 +0530

    Example 1

--- chapter_3/bicycles.py
+++ chapter_3/bicycles.py
@@ -1,2 +1,14 @@
 bicycles = ['trek', 'cannodale', 'redline', 'specialized']
-print(bicycles)
+
+# Index Position starts at 0
+print(bicycles[0])
+print(bicycles[0].title())
+
+# To access last element from a list
+print(bicycles[-1])
+
+message = f"My first bicycle was a {bicycles[0].title()}"
+print(message)
+
+print("-------------------")
+
