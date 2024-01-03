7359cdd5d29d88f139885cd65c71a0b99bc940cc
Hemant <hemishra555@gmail.com>
2024-01-03 10:49:57 +0530

    looping lists

---
+++ chapter_4/magicians.py
@@ -0,0 +1,5 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick")
+    print(f"I can't wait for another trick, {magician.title()}.\n")
+print("Thank you. That was a great show!")
