2cbbfa412daa24e5365837f475b4f2528b3b7672
Hemant <hemishra555@gmail.com>
2024-01-03 11:21:01 +0530

    range function

---
+++ chapter_4/numbers.py
@@ -0,0 +1,11 @@
+# range(): 1st argument inclusive and last argument exclusive
+for value in range(1, 5):
+    print(value)    
+print("---------------")
+for value in range(1,6):
+    print(value)
+print("---------------")
+
+# Use range() to create a List of numbers
+numbers = list(range(1,6))
+print(numbers)
---
+++ chapter_4/square_num.py
@@ -0,0 +1,14 @@
+squares = []
+for value in range(1, 11):
+    # square = value ** 2
+    # squares.append(square)
+    squares.append(value ** 2)
+
+print(squares)
+
+# Statistics with a List of Numbers
+print(min(squares))
+
+print(max(squares))
+
+print(sum(squares))
